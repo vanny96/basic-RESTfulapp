ff05533c5f4b719ad07e21a470de9a70ffb82649
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-06-30 23:11:34 +0200

    first commit

---
+++ src/main/java/com/vanny96/UserPosts/UserPostsApplication.java
@@ -0,0 +1,13 @@
+package com.vanny96.UserPosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanny96/UserPosts/UserPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanny96.UserPosts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
